35e64831459fc3520821a27cda305378fa5183b6
morgantstewart <morgan@paysigil.com>
2025-07-09 11:10:13 -0400

    morning fixes july 9 1110AM

--- js/app.js
+++ js/app.js
@@ -3,56 +3,20 @@
 
 //9 pairs of symbols, 16 total used
 
-const symbols = ['✚', '✦', '✩', '✱', '✿', '☺', '☾', '♥', '◐']
-const icons = [...symbols, ...symbols]
+const cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let firstCardClicked 
+let secondCardClicked
+
+// ['✚', '✦', '✩', '✱', '✿', '☺', '☾', '♥', '◐']
+// const icons = [...symbols, ...symbols]
 
 
 
 
+const cardsEl = document.querySelectorAll(".sqr");
+console.log(cardsEl);
+const reset = document.querySelector("reset");
 
-
-// const gameContainer = document.getElementById('game');
-//     let firstCard = null;
-//     let lockBoard = false;
-
-
-// //define variable for user's first symbol choice:
-// let firstSymbolChoice = '';
-// const heavyGreekCross = '\u{&#10010}';
-// const blackFourPointedStar = '\u{&#10022}';
-// const stressOutlinedWhiteStar = '\u{&#10010}';
-// const heavyAsterisk = '\u{&#10033}';
-// const blackFlorette = '\u{&#10047}';
-// const whiteSmilingFace = '\u{&#9786}';
-// const lastQuarterMoon = '\u{&#9790}';
-// const blackHeartSuit = '\u{&#9829}';
-// const circleLeftBlack = '\u{&#9680}';
-
-// //define variable for user's second symbol choice:
-// let secondSymbolChoice = '';
-// const heavyGreekCross = '\u{&#10010}';
-// const blackFourPointedStar = '\u{&#10022}';
-// const stressOutlinedWhiteStar = '\u{&#10010}';
-// const heavyAsterisk = '\u{&#10033}';
-// const blackFlorette = '\u{&#10047}';
-// const whiteSmilingFace = '\u{&#9786}';
-// const lastQuarterMoon = '\u{&#9790}';
-// const blackHeartSuit = '\u{&#9829}';
-// const circleLeftBlack = '\u{&#9680}';
-
-
-
-// const winningCombos = [
-//     [heavyGreekCross, heavyGreekCross],
-//     [blackFourPointedStar, blackFourPointedStar],
-//     [stressOutlinedWhiteStar, stressOutlinedWhiteStar],
-//     [heavyAsterisk, heavyAsterisk],
-//     [blackFlorette, blackFlorette],
-//     [whiteSmilingFace, whiteSmilingFace],
-//     [lastQuarterMoon, lastQuarterMoon],
-//     [blackHeartSuit, blackHeartSuit],
-//     [circleLeftBlack, circleLeftBlack],
-// ];
 
 
 // const message("Congratulations, you're psychic!");
@@ -86,7 +50,8 @@ function clicked(event) {
 }
 
 
-const banana = () => {
+
+const checkForMatch = () => {
     if (firstCardClicked === secondCardClicked) {
         console.log(event.target.innerText)//add what happens
     } else {
@@ -95,22 +60,26 @@ const banana = () => {
     }
     firstCardClicked = undefined
     secondCardClicked = undefined
-}
+    tries++
+
+};
 
 
 
-card.forEach(card) => {
-    card.addEventListener("click",(event) => {
+
+cardsEl.forEach((card) => {
+    card.addEventListener("click", (event) => {
         if (firstCardClicked === undefined) {
         firstCardClicked = event.target.innerText;
         console.log(firstCardClicked)
         } else {
             secondCardClicked = event.target.innerText;
             console.log(secondCardClicked)
-        } banana();
-        );
-    }
-    
+        };
+    })
+})
+
+
 
 
     //if fir
@@ -124,39 +93,19 @@ card.forEach(card) => {
 //     console.log(parentElement);
 // };
 
-function checkMatch() {
-variable 1st Option
-variable 2nd Option
-if firstOption.length === 0, then firstOption = .qrEls
-else if second option === 0, then secondOption = sqrEls
-then execute checkmatch function inside here 
-};
+// function checkMatch() {
+// variable 1st Option
+// variable 2nd Option
+// if firstOption.length === 0, then firstOption = .qrEls
+// else if second option === 0, then secondOption = sqrEls
+// then execute checkmatch function inside here 
+// };
 
 
 /*----------- Event Listeners ----------*/
 
-const cards = document.querySelectorAll(".sqr");
-console.log(cards);
-const reset = document.querySelector("reset");
-
-grid.addEventListener('click', () => {
-    console.log("grid click event listener works!");
-});
-
 
 
 resetBtnEl.onclick = (reset) => {
-    console.log("reset pressed");
+    console.log("reset pressed")
 }
-
-
-// squareEls.forEach(squareEls => {
-//     squareEls.addEventListener('click, handleClick')
-// })
-
-
-
-
-
-// cards.addEventListener('click');
-// console.log("card selected")
