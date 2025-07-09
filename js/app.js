79e341f6f100d4cef142e8908bf1c5e4e718d860
morgantstewart <morgan@paysigil.com>
2025-07-08 20:45:58 -0400

    push during TA hours July 8 845PM

--- js/app.js
+++ js/app.js
@@ -75,7 +75,7 @@ const squareEls = document.querySelectorAll('.sqr');
 const boardEls = document.querySelector('board')
 const messageEl = document.querySelector('#message');
 const resetBtnEl = document.querySelector('#reset');
-
+console.log(resetBtnEl);
 
 
 /*-------------- Functions -------------*/
@@ -85,6 +85,52 @@ function clicked(event) {
     console.log("clicked")
 }
 
+
+const banana = () => {
+    if (firstCardClicked === secondCardClicked) {
+        console.log(event.target.innerText)//add what happens
+    } else {
+        console.log("Not a match!") 
+        // add cards flipping back over
+    }
+    firstCardClicked = undefined
+    secondCardClicked = undefined
+}
+
+
+
+card.forEach(card) => {
+    card.addEventListener("click",(event) => {
+        if (firstCardClicked === undefined) {
+        firstCardClicked = event.target.innerText;
+        console.log(firstCardClicked)
+        } else {
+            secondCardClicked = event.target.innerText;
+            console.log(secondCardClicked)
+        } banana();
+        );
+    }
+    
+
+
+    //if fir
+//how to flip crds, shuffle cards, track pairs
+//how to demonstrate win?
+
+
+
+// function handleClick(event) {
+//     const parentElement = event.target.[parentElement];
+//     console.log(parentElement);
+// };
+
+function checkMatch() {
+variable 1st Option
+variable 2nd Option
+if firstOption.length === 0, then firstOption = .qrEls
+else if second option === 0, then secondOption = sqrEls
+then execute checkmatch function inside here 
+};
 
 
 /*----------- Event Listeners ----------*/
@@ -102,6 +148,11 @@ grid.addEventListener('click', () => {
 resetBtnEl.onclick = (reset) => {
     console.log("reset pressed");
 }
+
+
+// squareEls.forEach(squareEls => {
+//     squareEls.addEventListener('click, handleClick')
+// })
 
 
 
